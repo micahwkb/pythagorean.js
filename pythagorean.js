d532f9de734a4a481ba0f946a0715ed921925c6a
Micah Britton <micahwkb@gmail.com>
2016-11-03 12:31:45 -0400

    added pythagorean.js, no function build yet

---
+++ pythagorean.js
@@ -0,0 +1,14 @@
+/*
+calculate z given x and y
+*/
+
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map(/* Your code here */);
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
