b5f1981027406f539faa19093539fe5544166b3d
Micah Britton <micahwkb@gmail.com>
2016-11-03 12:54:15 -0400

    added callback function for input.map, also built alternate version using separate function

--- pythagorean.js
+++ pythagorean.js
@@ -7,8 +7,20 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+var result = input.map(function(obj) {
+  return Math.sqrt(obj.x * obj.x + obj.y * obj.y);
+});
+
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
 console.log(result[2] === 17);
+
+// Alternately:
+/*
+var result = input.map(pythCalculator);
+
+function pythCalculator(obj) {
+  return Math.sqrt(obj.x * obj.x + obj.y * obj.y);
+}
+*/
